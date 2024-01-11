c5b846d2af01741132c177174bb6959b9a65c30c
Alexander Morozov <amorozov@csas.cz>
2024-01-11 11:10:11 +0100

    commit 5
    Signed-off-by: Alexander Morozov <amorozov@csas.cz>

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 class GildedRose {
 
     final static int QUALITY_GROWTH_THRESHOLD = 50;
+    final static int QUALITY_DECREASE_THRESHOLD = 0;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -13,7 +14,7 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
+                if (item.quality > QUALITY_DECREASE_THRESHOLD) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
                         item.quality--;
                 }
@@ -41,11 +42,11 @@ class GildedRose {
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0)
+                        if (item.quality > QUALITY_DECREASE_THRESHOLD)
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
                                 item.quality--;
                     } else
-                        item.quality = 0;
+                        item.quality = QUALITY_DECREASE_THRESHOLD;
                 } else if (item.quality < QUALITY_GROWTH_THRESHOLD)
                     item.quality++;
             }
