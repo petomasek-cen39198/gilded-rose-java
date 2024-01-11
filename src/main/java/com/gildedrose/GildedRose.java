b3f96d42df6ed74da1f2bba4c7fb6033f50d99c7
Alexander Morozov <amorozov@csas.cz>
2024-01-11 11:02:31 +0100

    commit 3
    Signed-off-by: Alexander Morozov <amorozov@csas.cz>

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,21 +13,21 @@ class GildedRose {
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-                        item.quality = item.quality - 1;
+                        item.quality--;
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality++;
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50)
-                                item.quality = item.quality + 1;
+                                item.quality++;
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50)
-                                item.quality = item.quality + 1;
+                                item.quality++;
                         }
                     }
                 }
@@ -41,12 +41,11 @@ class GildedRose {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0)
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-                                item.quality = item.quality - 1;
+                                item.quality--;
                     } else
                         item.quality = 0;
                 } else if (item.quality < 50)
-                    item.quality = item.quality + 1;
+                    item.quality++;
             }
         }
-    }
-}
+    }}
