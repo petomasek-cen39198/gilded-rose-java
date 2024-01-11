c0eac3a1601a27579087a13d5fc34c3a1e80d2c5
Alexander Morozov <amorozov@csas.cz>
2024-01-11 12:24:58 +0100

    mnam commit 11
    Signed-off-by: Alexander Morozov <amorozov@csas.cz>

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,22 +14,7 @@ class GildedRose {
         for (Item item : items) {
             if (item.name.equals("Sulfuras, Hand of Ragnaros")) continue;
 
-            int adjustment = 0;
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (isExpired(item))
-                    adjustment = -item.quality;
-                else if (item.sellIn < 6)
-                    adjustment = 3;
-                else if (item.sellIn < 11)
-                    adjustment = 2;
-                else
-                    adjustment = 1;
-
-            } else if (item.name.equals("Aged Brie")) {
-                adjustment = isExpired(item) ? 2 : 1;
-            } else {
-                adjustment = isExpired(item) ? -2 : -1;
-            }
+            int adjustment = getAdjustment(item);
 
             if (adjustment > 0)
                 item.quality = Math.min(item.quality + adjustment, QUALITY_GROWTH_THRESHOLD);
@@ -38,6 +23,26 @@ class GildedRose {
 
             item.sellIn = item.sellIn - 1;
         }
+    }
+
+    private static int getAdjustment(Item item) {
+        int adjustment = 0;
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (isExpired(item))
+                adjustment = -item.quality;
+            else if (item.sellIn < 6)
+                adjustment = 3;
+            else if (item.sellIn < 11)
+                adjustment = 2;
+            else
+                adjustment = 1;
+
+        } else if (item.name.equals("Aged Brie")) {
+            adjustment = isExpired(item) ? 2 : 1;
+        } else {
+            adjustment = isExpired(item) ? -2 : -1;
+        }
+        return adjustment;
     }
 
     private static boolean isExpired(Item item) {
